ea72996dd8aab4ae13c80f1317edf056b125e9d5
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-15 11:47:52 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,8 +28,7 @@ false
 end
 
 def full?(board)
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  [board].all? do |full|
+  board.all? do |input|
     if [board] == "X" || [board] == "O"
   end
 end
